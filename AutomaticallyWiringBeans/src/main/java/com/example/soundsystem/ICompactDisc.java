e467e42c72d416c51b1911c8eb0563abec3c69d7
thongdhse160015 <111677503+thongdhse160015@users.noreply.github.com>
2023-06-07 16:10:30 +0700

    Add files via upload

---
+++ AutomaticallyWiringBeans/src/main/java/com/example/soundsystem/ICompactDisc.java
@@ -0,0 +1,5 @@
+package com.example.soundsystem;
+
+public interface ICompactDisc {
+    void play();
+}
---
+++ AutomaticallyWiringBeans/src/main/java/com/example/soundsystem/SoundsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.soundsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundsystemApplication.class, args);
+	}
+
+}
